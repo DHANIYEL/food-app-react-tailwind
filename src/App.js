fc5360b808b988d465d93dc114612a131c3f98c5
DHANIYEL <dhaniyeldarvesh7@gmail.com>
2023-11-27 00:19:26 +0530

    categories

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import NavBar from "./components/NavBar";
 import Hero from "./components/Hero";
 import HeadLineCards from "./components/HeadLineCards";
 import FoodsCategory from "./components/FoodsCategory";
+import Category from "./components/Category";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Hero />
       <HeadLineCards />
       <FoodsCategory />
+      <Category />
     </div>
   );
 }
